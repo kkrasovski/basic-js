7e5a8fcf6eb302324e7b982c7b63ef3ed08150d9
Kiryl Krasouski <ziksow@gmail.com>
2021-03-03 23:45:58 +0300

    feat: add simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chainOutput: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainOutput.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainOutput.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof (position) !== 'number' || position < 1 || this.chainOutput[position] === undefined) {
+      this.chainOutput = [];
+      throw new Error('Error with remove link');
+    } else {
+      this.chainOutput.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
-
 module.exports = chainMaker;
+
+
