b4327ca391d57f151f2dafe2f48e298a14ed4fae
Kiryl Krasouski <ziksow@gmail.com>
2021-03-03 08:58:03 +0300

    feat: add hanoi-tower and what-season tasks

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+if (date) {
+if (isNaN(date.getTime())) {
+  throw new CustomError('Unable to determine the time of year!');
+} else {
+  let mounthNumber = date.getMonth();
+  if(mounthNumber == 11 || mounthNumber == 0 || mounthNumber == 1) {  
+     return "winter"
+  }
+  if(mounthNumber == 2 || mounthNumber == 3 || mounthNumber == 4) {   
+     return "spring"
+  }
+  if(mounthNumber == 5 || mounthNumber == 6 || mounthNumber == 7) { 
+     return "summer"
+  }
+  if(mounthNumber == 8 || mounthNumber == 9 || mounthNumber == 10) { 
+    return "autumn"
+  }
+}
+} else {
+  return "Unable to determine the time of year!";
+}
+}
