476cd1747ce0fab7d00ceaa4fcad94b2fe6befa6
Kiryl Krasouski <ziksow@gmail.com>
2021-03-03 12:40:11 +0300

    feat: add resursive-pepth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (Array.isArray(arr)) {
+      let level = 0;
+      level = Math.max(
+        level,
+        ...arr.map((sublevel) => this.calculateDepth(sublevel))
+      );
+      return level + 1;
+    } else {
+      return 0;
+    }
   }
 };
